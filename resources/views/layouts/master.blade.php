922e5ef6da4bf38224af64aeffac3924231fd54e
kafyar2022 <kafyar2022@gmail.com>
2022-04-25 06:56:10 +0500

    first commit

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>@yield('title')</title>
+  <link rel="stylesheet" href="{{ asset('css/normalize.css') }}">
+  <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+</head>
+
+<body>
+  @include('layouts.header')
+
+  @yield('content')
+
+  @include('layouts.footer')
+
+  @yield('script')
+</body>
+
+</html>
---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.master')
+
+@section('title', 'Lady Healthcare')
+
+@section('content')
+  <main class="container">
+
+  </main>
+@endsection
+
+@section('script')
+  {{-- script --}}
+@endsection
